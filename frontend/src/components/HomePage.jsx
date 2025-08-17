841c160d7cd31135ad41d3d337bdc9177b3791d1
Aqib <shihanaqib@gmail.com>
2025-08-17 14:34:54 +0000

    Messing around remmbering compoenents styling etc

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function HomePage() {
+  return (
+    <>
+      <h1> This is the Home Page</h1>
+      <p>text goes here</p>
+      <button> Click </button>
+    </>
+  );
+}
+
+export default HomePage;
