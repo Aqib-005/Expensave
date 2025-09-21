33666f0ae7fa6547a9345c6f0da30cf6a69ed6c3
Aqib <shihanaqib@gmail.com>
2025-09-21 15:02:54 +0000

    Trying to deploy 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy"; // ✅ you missed this
 
 export default defineConfig({
   server: {
@@ -7,8 +9,24 @@ export default defineConfig({
       "/api": "https://expensave-a8zd.onrender.com",
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_redirects", // ✅ copy redirects
+          dest: ".", // to dist root
+        },
+      ],
+    }),
+  ],
+  base: "/", // ✅ no /Expensave anymore since you’re on Render
   build: {
-    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
   },
+  publicDir: "public",
 });
